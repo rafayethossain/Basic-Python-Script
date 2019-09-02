f4fbf0201eac7d569ce5776eebe731326ef5c4db
rafayethossain <rafayet14@gmail.com>
2019-09-02 23:20:41 +0600

    adding files

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\t I am tabben in."
+persian_cat = "I'm split \non line."
+backslash_cat = "I am \\ a \\ cat."
+
+fat_cat= """
+I will do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
