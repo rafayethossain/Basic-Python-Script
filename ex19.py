2bb895a67e6fe9cbdabbae054808ebf77e00ba0a
rafayethossain <rafayet14@gmail.com>
2019-09-05 22:22:27 +0600

    changes names

--- ex19.py
+++ ex19.py
@@ -1,6 +1,6 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print("You have %d cheeses!" %cheese_count)
-    print("You have %d boxes of crackers!" %boxes_boxes_crackers)
+    print("You have %d cheeses!" % cheese_count)
+    print("You have %d boxes of crackers!" % boxes_of_crackers)
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
@@ -14,7 +14,7 @@ amount_of_crackers = 50
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 print("We can even do math inside too:")
-cheese_and_crackers(10+20, 5+6)
+cheese_and_crackers(10 + 20, 5 + 6)
 
 print("And we can combine two, variables and math:")
 
