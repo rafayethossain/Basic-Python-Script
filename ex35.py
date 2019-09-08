dfd51421f565cf2085ea8ac583586e338f076b00
rafayethossain <rafayet14@gmail.com>
2019-09-08 21:59:46 +0600

    Doing Things to Lists

--- ex35.py
+++ ex35.py
@@ -26,10 +26,10 @@ def bear_room():
     while True:
         next = input("> ")
 
-        if next == "take hone":
+        if next == "take honey":
             dead("The bear looks at you then slaps you face off.")
         elif next == "taunt bear" and not bear_moved:
-            dear("The bear has moved from the door. You can go through it now.")
+            dead("The bear has moved from the door. You can go through it now.")
             bear_moved = True
         elif next == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews you leg off.")
