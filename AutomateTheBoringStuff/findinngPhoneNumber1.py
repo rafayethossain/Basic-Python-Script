0cf1e1f92755405e296d0e9ad3049d3eca0d5e36
rafayethossain <rafayet14@gmail.com>
2019-09-22 21:43:55 +0600

    reg ex

---
+++ AutomateTheBoringStuff/findinngPhoneNumber1.py
@@ -0,0 +1,27 @@
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+message = 'Call me 415-555-1013 tomorrow or at 15-555-9999'
+foundNumber = False
+
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: '+ chunk)
+        foundNumber = True
+if not foundNumber:
+    print('Could not find any phone numbers.')
+
---
+++ AutomateTheBoringStuff/findinngPhoneNumber2.py
@@ -0,0 +1,10 @@
+import re
+message = 'Call me 415-555-1013 tomorrow or at 15-555-9999'
+
+phoneNumberRegx = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumberRegx.findall(message) # all instance
+
+print(mo)
+
+
+# Using regualar expression
